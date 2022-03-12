48d4d99926aa4398384336124eb48d5c7955598d
seunghee jin <sualng123@gmail.com>
2022-03-13 01:10:05 +0900

    start scrapy

--- ecommerce/ecommerce/spiders/gmarket.py
+++ ecommerce/ecommerce/spiders/gmarket.py
@@ -3,7 +3,8 @@ import scrapy
 
 class GmarketSpider(scrapy.Spider):
     name = 'gmarket'
-    allowed_domains = ['www.gmarket.co.kr']
+    allowed_domains = ['www.gmarket.co.kr'] # 다른 사이트들을 크롤링 하는 것을 막겠다.\
+        # 해당 사이트 주소가 포함 된 경우에만 크롤링 하겠다.
     start_urls = ['http://www.gmarket.co.kr/']
 
     def parse(self, response):
